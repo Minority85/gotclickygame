ec7cb38bd0b46da852340dfad4ea823c4761748d
Daniel Bryant <azbryant85@gmail.com>
2019-06-28 21:29:55 -0700

    Updates

---
+++ precache-manifest.d88978e001478ed33502d263915f6928.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35a1d9783545cdc54560a78644ea9c2c",
+    "url": "/gotclickygame/index.html"
+  },
+  {
+    "revision": "9e6cbc06e6b41eabd0fd",
+    "url": "/gotclickygame/static/css/main.a6b373d4.chunk.css"
+  },
+  {
+    "revision": "eb6981577fd2f9081f53",
+    "url": "/gotclickygame/static/js/2.9a853c1d.chunk.js"
+  },
+  {
+    "revision": "9e6cbc06e6b41eabd0fd",
+    "url": "/gotclickygame/static/js/main.79b7352c.chunk.js"
+  },
+  {
+    "revision": "277214aa8844d84aaf3d",
+    "url": "/gotclickygame/static/js/runtime~main.e83a6d2d.js"
+  }
+]);
